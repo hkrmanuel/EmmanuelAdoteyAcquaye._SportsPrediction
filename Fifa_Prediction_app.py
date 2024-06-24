862283698d7ee67ab47ae648c8921c47539e946c
hkrmanuel <hkrmanuel2.0@gmail.com>
2024-06-23 18:16:42 -1200

    Submission Of Regression Assignment

---
+++ Fifa_Prediction_app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load the scalers and model
+model = joblib.load('Fifa_Regression_Model.pkl')
+
+
+st.title("FIFA Player Rating Predictor")
+
+# Input variables
+input_var = ['movement_reactions', 'potential', 'passing', 'wage_eur', 'value_eur', 'dribbling']
+inputs = {}
+
+
+# Collect input data from the user
+for feature in input_var:
+    inputs[feature] = st.number_input(f"Enter {feature}", value=0.0)
+
+
+# Predict button
+if st.button("Predict"):
+    input_df = pd.DataFrame([inputs])
+    print(input_df)
+    # Make prediction
+    prediction = model.predict(input_df)
+    
+    # Display prediction and confidence interval
+    st.write(f"Predicted Overall Rating: {prediction[0]:.2f}")
+
